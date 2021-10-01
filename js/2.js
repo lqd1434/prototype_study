200b33d8c8fd68dfd725da3ea6e10c6ef4751c78
lqd1434 <1434288209@qq.com>
2021-10-01 18:45:41 +0800

    保存

--- js/2.js
+++ js/2.js
@@ -9,6 +9,35 @@ class Person{
 	}
 }
 
+Person.prototype.id = 1
+
+class Student extends Person{
+	sayHello() {
+		console.log(`我是${this.name},我今年${this.age}岁`,'我是学生')
+	}
+
+	test(){
+		console.log(super.id)
+		//super调用的是父类的prototype
+	}
+}
+
+console.log(Object.getPrototypeOf(Person))
+console.log(Person.prototype)
+console.log(Object.getPrototypeOf(Student));
+
+console.log(typeof Person) //=> function
+console.log(typeof Student) //=> function
+
 const jack = new Person('jack',18)
+const bob =new Student('jack',18)
+
 console.log(jack)
-console.log(Object.getPrototypeOf(jack) === Person.prototype)
+console.log(bob)
+
+bob.test()
+
+console.log(Object.getPrototypeOf(Student));
+console.log(Student.prototype)
+console.log(Person.prototype)
+
